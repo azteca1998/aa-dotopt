b5c2ce7125d5a7d74c7bc1d8e8974aba32ac4e9c
Esteve Soler Arderiu <soler.arderiu@gmail.com>
2021-12-13 02:33:12 +0000

    Created documentation project.

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,5 +2,5 @@
 
 
 sudo apt install -y \
-    bash-completion build-essential git cmake cmake-extras libpfm4-dev \
-    libpython3-dev python3
+    bash-completion build-essential git cmake cmake-extras latexmk libpfm4-dev \
+    libpython3-dev python3 texlive
